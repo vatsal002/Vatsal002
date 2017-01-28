153c326f62265db06aa4e2639aa3a26c726f79a7
Mahant Tech <mahanttechconsulting@gmail.com>
2017-01-28 17:55:28 +0530

    asdasd

---
+++ TESTMVC/TESTMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TESTMVC.Startup))]
+namespace TESTMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
